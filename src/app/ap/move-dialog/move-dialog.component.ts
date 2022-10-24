1b6ac2fc0fbefdce5ba1bfccdc8dde5db4a0f1f0
Hartmut Bauer <hb42@gmx.de>
2022-10-24 21:54:48 +0200

    sbsdb #46 HW/TAG verschieben, Handling Fremde HW

--- src/app/ap/move-dialog/move-dialog.component.ts
+++ src/app/ap/move-dialog/move-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Inject, OnInit } from "@angular/core";
+import { ChangeDetectorRef, Component, EventEmitter, Inject, OnInit } from "@angular/core";
 import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import { environment } from "../../../environments/environment";
@@ -7,6 +7,11 @@ import { FormFieldErrorStateMatcher } from "../../shared/form-field-error-state-
 import { StringCompare } from "../../shared/helper";
 import { Arbeitsplatz } from "../../shared/model/arbeitsplatz";
 import { MoveData } from "./move-data";
+
+interface MoveValue {
+  value: number;
+  text: string;
+}
 
 @Component({
   selector: "sbsdb-move-dialog",
@@ -21,26 +26,21 @@ export class MoveDialogComponent implements OnInit {
   public apCtrl: FormControl;
   public moveCtrl: FormControl;
   public apSelect: Arbeitsplatz[];
-  public moveSelect: { value: number; text: string }[];
+  public moveSelect: MoveValue[];
+
+  private fremdeHwSource = false;
+  private fremdeHwTarget = false;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: MoveData,
     public matDialogRef: MatDialogRef<MoveDialogComponent>,
     public formBuilder: FormBuilder,
-    private dataService: DataService
+    private dataService: DataService,
+    private cdRef: ChangeDetectorRef
   ) {
     if (!environment.production) console.debug(`c'tor ${this.constructor.name}`);
     this.formGroup = this.formBuilder.group({});
-
-    this.apSelect = this.dataService.apList
-      .filter((a) => a.apKatId === this.data.ap.apKatId && a.apId !== this.data.ap.apId)
-      .sort((a, b) => StringCompare(a.apname, b.apname));
-    this.apSelect.unshift(null);
-    this.moveSelect = [];
-    this.moveSelect.push(null);
-    this.moveSelect.push({ value: 1, text: "Hardware" });
-    this.moveSelect.push({ value: 2, text: "Sonstige Informationen" });
-    this.moveSelect.push({ value: 3, text: "Hardware + Sonstige Informationen" });
+    this.fremdeHwSource = this.dataService.isFremderAp(data.ap);
   }
 
   ngOnInit(): void {
@@ -50,11 +50,26 @@ export class MoveDialogComponent implements OnInit {
     // eslint-disable-next-line @typescript-eslint/unbound-method
     this.moveCtrl = new FormControl(null, [Validators.required]);
     this.formGroup.addControl("movw", this.moveCtrl);
+
+    this.apSelect = this.dataService.apList
+      .filter((a) => a.apKatId === this.data.ap.apKatId && a.apId !== this.data.ap.apId)
+      .sort((a, b) => StringCompare(a.apname, b.apname));
+    this.apSelect.unshift(null);
+    this.mkMoveSelect();
+  }
+
+  public targetChange(): void {
+    this.fremdeHwTarget = this.dataService.isFremderAp(this.apCtrl.value as Arbeitsplatz);
+    this.mkMoveSelect();
+    // Das Aendern der selectList aendert den Status der formControl
+    // und das triggert *ExpressionChangedAfterItHasBeenCheckedError*.
+    // Das folgende verhindert diesen Fehler.
+    this.cdRef.detectChanges();
   }
 
   public onSubmit() {
     this.data.target = this.apCtrl.value as Arbeitsplatz;
-    this.data.what = (this.moveCtrl.value as { value: number; text: string }).value;
+    this.data.what = (this.moveCtrl.value as MoveValue).value;
   }
 
   public getErrorMessage(control: FormControl): string {
@@ -62,5 +77,30 @@ export class MoveDialogComponent implements OnInit {
       return "Das Feld darf nicht leer sein.";
     }
     return null;
+  }
+
+  private mkMoveSelect() {
+    const old: number = this.moveCtrl.value ? (this.moveCtrl.value as MoveValue).value : 0;
+
+    this.moveSelect = [];
+    this.moveSelect.push(null);
+    if (!this.fremdeHwSource && !this.fremdeHwTarget) {
+      this.moveSelect.push({ value: 1, text: "Hardware" });
+    }
+    this.moveSelect.push({ value: 2, text: "Sonstige Informationen" });
+    if (!this.fremdeHwSource && !this.fremdeHwTarget) {
+      this.moveSelect.push({ value: 3, text: "Hardware + Sonstige Informationen" });
+    }
+
+    if (old) {
+      const val = this.moveSelect.find((m) => (m ? m.value === old : false));
+      if (val) {
+        this.moveCtrl.setValue(val);
+      } else {
+        this.moveCtrl.setValue(null);
+      }
+    } else {
+      this.moveCtrl.setValue(null);
+    }
   }
 }
